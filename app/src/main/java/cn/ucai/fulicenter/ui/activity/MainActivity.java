beb2fb374d5760cab7d0dfce547c22c7a7c7b34a
songjiaqi007 <609208788@qq.com>
2017-03-14 19:14:51 +0800

    使用ButteKnife生成控件

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -3,12 +3,14 @@ package cn.ucai.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
 
 /**
@@ -30,11 +32,25 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup rg;
     @BindView(R.id.fl)
     FrameLayout fl;
+    Unbinder bind;
+    int index = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        bind = ButterKnife.bind(this);
+    }
+
+    public void onCheckedChange(View view) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind != null) {
+            bind.unbind();
+        }
     }
 }
