e6406d101372edb1073110b151e053dfd481cac8
songjiaqi007 <609208788@qq.com>
2017-03-15 23:07:35 +0800

    补提交BoutiqueModel和IBoutiqueModel

---
+++ app/src/main/java/cn/ucai/fulicenter/model/net/IBoutiqueModel.java
@@ -0,0 +1,15 @@
+package cn.ucai.fulicenter.model.net;
+
+import android.content.Context;
+
+import cn.ucai.fulicenter.model.bean.BoutiqueBean;
+import cn.ucai.fulicenter.model.bean.NewGoodsBean;
+
+/**
+ * Created by liuning on 2017/3/15.
+ */
+
+public interface IBoutiqueModel {
+    void loadData(Context context, OnCompleteListener<BoutiqueBean[]> listener);
+
+}
