544f660a05480983df6b1d4496b02b358209af35
songjiaqi007 <609208788@qq.com>
2017-03-15 09:59:14 +0800

    数据层添加netINewGoodsModel,NewGoodsModel,OnCompleteListener

---
+++ app/src/main/java/cn/ucai/fulicenter/model/net/INewGoodsModel.java
@@ -0,0 +1,12 @@
+package cn.ucai.fulicenter.model.net;
+
+import android.content.Context;
+
+/**
+ * Created by liuning on 2017/3/15.
+ */
+
+public interface INewGoodsModel {
+    void loadData(Context context, int pageId, OnCompleteListener listener);
+
+}
