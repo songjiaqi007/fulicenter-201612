c02f5d080cb47ec6cd1689a54af00fb6b1f4a39e
songjiaqi007 <609208788@qq.com>
2017-03-20 23:13:29 +0800

    用户登录

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/LoginActivity.java
@@ -1,9 +1,11 @@
 package cn.ucai.fulicenter.ui.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,7 +13,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IUserModel;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.net.UserModel;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.model.utils.MD5;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
 import cn.ucai.fulicenter.ui.view.MFGT;
 
 /**
@@ -23,11 +34,15 @@ public class LoginActivity extends AppCompatActivity {
     EditText mUsername;
     @BindView(R.id.password)
     EditText mPassword;
+    String username, password;
+    IUserModel mModel;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mModel = new UserModel();
         ButterKnife.bind(this);
     }
 
@@ -47,15 +62,80 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
+        if (checkInput()) {
+            showDialog();
+            mModel.login(LoginActivity.this, username, MD5.getMessageDigest(password),
+                    new OnCompleteListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            Result result = ResultUtils.getListResultFromJson(s, User.class);
+                            if (result != null) {
+                                if (result == null && result.isRetMsg()) {
+                                    User user = (User) result.getRetData();
+                                    if (user != null) {
+                                        loginSuccess(user);
+                                    }
+                                } else {
+                                    if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                                        CommonUtils.showShortToast(R.string.login_fail_unknow_user);
+                                    }
+                                    if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                                        CommonUtils.showShortToast(R.string.login_fail_error_password);
+                                    }
+                                }
+                            }
+                            pd.dismiss();
+                        }
 
+
+                        @Override
+                        public void onError(String error) {
+                            CommonUtils.showShortToast(R.string.login_fail);
+                            pd.dismiss();
+                        }
+                    });
+        }
+    }
+
+    private void loginSuccess(User user) {
+        FuLiCenterApplication.setCurrentUser(user);
+    }
+
+    private void showDialog() {
+        pd = new ProgressDialog(LoginActivity.this);
+        pd.setMessage(getString(R.string.registering));
+        pd.show();
+    }
+
+
+    private boolean checkInput() {
+        username = mUsername.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            mUsername.requestFocus();
+            mUsername.setError(getString(R.string.user_name_connot_be_empty));
+            return false;
+        }
+        if (!username.matches("[a-zA-Z]\\w{5,15}")) {
+            mUsername.requestFocus();
+            mUsername.setError(getString(R.string.illegal_user_name));
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            mPassword.requestFocus();
+            mPassword.setError(getString(R.string.password_connot_be_empty));
+            return false;
+        }
+        return true;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode== I.REQUEST_CODE_REGISTER) {
+        if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_REGISTER) {
             String username = data.getStringExtra(I.User.USER_NAME);
             mUsername.setText(username);
         }
     }
 }
+
