603f85dcbb78de6b2e52fbc9e9674aff3d84c1b3
songjiaqi007 <609208788@qq.com>
2017-03-24 13:17:22 +0800

    添加购物车的Fragment并显示

---
+++ app/src/main/java/cn/ucai/fulicenter/ui/fragment/CartFragment.java
@@ -0,0 +1,34 @@
+package cn.ucai.fulicenter.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.model.net.CartModel;
+import cn.ucai.fulicenter.model.net.ICartModel;
+
+/**
+ * Created by liuning on 2017/3/24.
+ */
+
+public class CartFragment extends Fragment {
+    ICartModel mModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mModel = new CartModel();
+
+    }
+}
